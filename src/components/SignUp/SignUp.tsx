f5348e9090afec8e8b2fb96c0e9b34cc39021d8b
liuyong <564447023@qq.com>
2020-01-19 23:38:12 +0800

    添加axios请求及响应拦截器，完成 SignUp 组件

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState, ChangeEvent } from "react";
+import { Input, Icon, Button } from "antd";
+import axios from "../../config/axios";
+import { RouteChildrenProps, Link } from "react-router-dom";
 
-const SignUp = () => {
-  return <div>SignUp</div>;
+import "./SignUp.scss";
+
+const SignUp = (props: RouteChildrenProps) => {
+  const [account, setAccount] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+  const onChangeAccount = (e: ChangeEvent<HTMLInputElement>) => {
+    setAccount(e.target.value || "");
+  };
+  const onChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value || "");
+  };
+  const onChangePasswordConfirmation = (e: ChangeEvent<HTMLInputElement>) => {
+    setPasswordConfirmation(e.target.value || "");
+  };
+
+  const submit = async () => {
+    const URL = "sign_up/user";
+    try {
+      await axios.post(URL, {
+        account,
+        password,
+        password_confirmation: passwordConfirmation
+      });
+      console.log("注册成功");
+    } catch (error) {
+      console.error("提交注册用户密码失败");
+    }
+  };
+
+  return (
+    <div className="SignUp" id="SignUp">
+      <h1>土豆番茄闹钟-注册</h1>
+      <Input
+        placeholder="请输入用户名"
+        prefix={<Icon type="user" />}
+        value={account}
+        onChange={onChangeAccount}
+      />
+      <Input.Password
+        placeholder="请输入密码"
+        value={password}
+        onChange={onChangePassword}
+      />
+      <Input.Password
+        placeholder="请确认密码"
+        value={passwordConfirmation}
+        onChange={onChangePasswordConfirmation}
+      />
+      <Button block type="primary" onClick={submit}>
+        注册
+      </Button>
+      <p>
+        如果已有账号，请<Link to="login">登录</Link>
+      </p>
+    </div>
+  );
 };
 
 export default SignUp;
---
+++ src/config/axios.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const appID = "HsjvVF7uMwTNhonJVu7MgwAZ";
+const appSecret = "zQYB3yzmQXW4AMvEjh36LM1j";
+const baseURL = "https://gp-server.hunger-valley.com/";
+
+const instance = axios.create({
+  baseURL,
+  headers: {
+    "t-app-id": appID,
+    "t-app-secret": appSecret
+  }
+});
+
+// 添加请求拦截器
+instance.interceptors.request.use(
+  config => {
+    const xToken = localStorage.getItem("x-token");
+    if (xToken) {
+      config.headers["Authorization"] = `Bearer ${xToken}`;
+    }
+    return config;
+  },
+  error => {
+    console.error("axios add request interceptor error", error);
+    return Promise.reject(error);
+  }
+);
+
+// 添加相应拦截器
+instance.interceptors.response.use(
+  response => {
+    if (response.headers["x-token"]) {
+      localStorage.setItem("x-token", response.headers["x-token"]);
+    }
+    return response;
+  },
+  error => {
+    console.error("axios add response interceptor error", error);
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
