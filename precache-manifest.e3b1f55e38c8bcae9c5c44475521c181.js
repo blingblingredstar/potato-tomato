cfe7d232f4038915f9566776235153a0676cf0f5
liuyong <564447023@qq.com>
2020-01-26 20:31:55 +0800

    Updates

---
+++ precache-manifest.e3b1f55e38c8bcae9c5c44475521c181.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72799ff469397367e0479ddbb2dccb18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c458e9817d896d8bc59b",
+    "url": "/static/css/2.2e96121e.chunk.css"
+  },
+  {
+    "revision": "1003c90cb2cea0f0eb56",
+    "url": "/static/css/main.3f215d8c.chunk.css"
+  },
+  {
+    "revision": "c458e9817d896d8bc59b",
+    "url": "/static/js/2.b3d6fda7.chunk.js"
+  },
+  {
+    "revision": "22a30969a14a9411944ddae482876d82",
+    "url": "/static/js/2.b3d6fda7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1003c90cb2cea0f0eb56",
+    "url": "/static/js/main.140571fc.chunk.js"
+  },
+  {
+    "revision": "467882ced63b7b3c3984",
+    "url": "/static/js/runtime-main.c70831a4.js"
+  },
+  {
+    "revision": "1a3cd3f99a9bfd11f7b89138f5d24327",
+    "url": "/static/media/logo.1a3cd3f9.svg"
+  }
+]);
