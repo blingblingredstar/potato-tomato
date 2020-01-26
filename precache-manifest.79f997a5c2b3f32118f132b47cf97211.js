a96687c3a61ddca86044de29dd465602639bf10d
liuyong <564447023@qq.com>
2020-01-26 20:41:06 +0800

    Updates

---
+++ precache-manifest.79f997a5c2b3f32118f132b47cf97211.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2016b7c9a1dd5d41fa044cecde2fec1",
+    "url": "/potato-tomato/index.html"
+  },
+  {
+    "revision": "8f93fe24004a069c9d1c",
+    "url": "/potato-tomato/static/css/2.2e96121e.chunk.css"
+  },
+  {
+    "revision": "a1cd3c71f2aea702264c",
+    "url": "/potato-tomato/static/css/main.3f215d8c.chunk.css"
+  },
+  {
+    "revision": "8f93fe24004a069c9d1c",
+    "url": "/potato-tomato/static/js/2.46b98ae6.chunk.js"
+  },
+  {
+    "revision": "22a30969a14a9411944ddae482876d82",
+    "url": "/potato-tomato/static/js/2.46b98ae6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a1cd3c71f2aea702264c",
+    "url": "/potato-tomato/static/js/main.28152dd8.chunk.js"
+  },
+  {
+    "revision": "307d06f63289be29cfc3",
+    "url": "/potato-tomato/static/js/runtime-main.e1b6b806.js"
+  },
+  {
+    "revision": "1a3cd3f99a9bfd11f7b89138f5d24327",
+    "url": "/potato-tomato/static/media/logo.1a3cd3f9.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3b1f55e38c8bcae9c5c44475521c181.js"
+  "/potato-tomato/precache-manifest.79f997a5c2b3f32118f132b47cf97211.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/potato-tomato/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
